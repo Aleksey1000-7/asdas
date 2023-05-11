884bd4c2437c73c8066bb39c6aa0a9fb6529b214
Алексей Незнахин <132550318+Aleksey1000-7@users.noreply.github.com>
2023-05-11 16:36:19 +0500

    Add files via upload

---
+++ style.py
@@ -0,0 +1,29 @@
+style = ("""
+         QMainWindow{
+            background: #3EB489;
+         }
+         QLaibel{
+            font-size: 44px;
+         }
+         QLaibel#lbl{
+            font-size: 10px;
+         }
+         QLaibel#lbl_sw{
+            font-size: 30px;
+         }
+         QPushButton{
+            font-size: 24px;
+            background: #008000;
+            border:1px;
+            border-radius: 20px;
+         }
+         QPushButton:hover{
+            font-size:30px;
+            background:#015D52;
+            border-radius: 20px;
+
+         }
+         QLineEdit{
+            font-size:25px;
+         }
+        """)
